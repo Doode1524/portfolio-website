546b2480d33910f695f4bed2701d1558bef3fe82
joey <doode1524@gmail.com>
2021-04-19 00:42:56 -0400

    init

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello Portfolio! My name is Joey 2 Slice</h1>
+        </div>
+    )
+}
+
+export default App
