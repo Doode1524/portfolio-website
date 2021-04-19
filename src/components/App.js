95280161d8699e5c64e62f5656d040582276d6fc
joey <doode1524@gmail.com>
2021-04-19 16:46:27 -0400

    app.css init

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import '../styles/App.css'
+import '../images/reactlogo.png'
 
 const App = () => {
     return (
-        <div>
+        <div className="app">
             <h1>Hello Portfolio! My name is Joey 2 Slice</h1>
         </div>
     )
