c7df9e61491afa54d6a1c57101539332561e48b9
joey <doode1524@gmail.com>
2021-04-20 15:38:44 -0400

    videos component init

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import CardMedia from '@material-ui/core/CardMedia';
+import Videos from './Videos'
+
 
 const Projects = () => {
     return (
         <div>
             <h1>Projects:</h1>
+            <Videos />
         </div>
     )
 }
