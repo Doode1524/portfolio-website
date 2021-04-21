27ff0cbdca3a0c9bd09c2b5336cb10809b0e8e9a
joey <doode1524@gmail.com>
2021-04-20 20:21:47 -0400

    render multiple video card component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,41 @@
 import React from 'react'
 import Videos from './Videos'
+import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
+
+    function createData(title, date, github, youtube, description ) {
+        return {title, date, github, youtube, description };
+      }
+      
+      const projects = [
+        createData('React BlackJack', 'April 2021', "https://github.com/Doode1524/ReactJack-frontend", "https://www.youtube.com/embed/NPp_AITIQ-M", "A casino style BlackJack game built using a Rails API backend, React/Redux frontend, and a 3rd party API"),
+        createData('React BlackJack', 'April 2021', "https://github.com/Doode1524/ReactJack-frontend", "https://www.youtube.com/embed/NPp_AITIQ-M", "A casino style BlackJack game built using a Rails API backend, React/Redux frontend, and a 3rd party API"),
+        createData('React BlackJack', 'April 2021', "https://github.com/Doode1524/ReactJack-frontend", "https://www.youtube.com/embed/NPp_AITIQ-M", "A casino style BlackJack game built using a Rails API backend, React/Redux frontend, and a 3rd party API"),
+        createData('React BlackJack', 'April 2021', "https://github.com/Doode1524/ReactJack-frontend", "https://www.youtube.com/embed/NPp_AITIQ-M", "A casino style BlackJack game built using a Rails API backend, React/Redux frontend, and a 3rd party API"),
+        createData('React BlackJack', 'April 2021', "https://github.com/Doode1524/ReactJack-frontend", "https://www.youtube.com/embed/NPp_AITIQ-M", "A casino style BlackJack game built using a Rails API backend, React/Redux frontend, and a 3rd party API"),
+        createData('React BlackJack', 'April 2021', "https://github.com/Doode1524/ReactJack-frontend", "https://www.youtube.com/embed/NPp_AITIQ-M", "A casino style BlackJack game built using a Rails API backend, React/Redux frontend, and a 3rd party API"),
+  
+      ];
+
+      const useStyles = makeStyles((theme) => ({
+        root: {
+          flexGrow: 1,
+        },
+      }));
 
 const Projects = () => {
+    const classes = useStyles();
+
     return (
-        <div>
+        <div className={classes.root}>
+            <Grid container>
             <h1>Projects:</h1>
-            <Videos />
-        </div>
+                <Grid container item xs={12} spacing={3}>
+                    {projects.map((project) => <Videos title={project.title} date={project.date} github={project.github} youtube={project.youtube} description={project.description} />)}
+                </Grid>
+            </Grid>
+        </div>  
     )
 }
 
--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,45 +1,52 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import { blue } from '@material-ui/core/colors';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { blue } from "@material-ui/core/colors";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import ShareIcon from "@material-ui/icons/Share";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    maxWidth: 345,
+    // maxWidth: 345,
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9
+    paddingTop: "56.25%", // 16:9
   },
   expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
       duration: theme.transitions.duration.shortest,
     }),
   },
   expandOpen: {
-    transform: 'rotate(180deg)',
+    transform: "rotate(180deg)",
   },
   avatar: {
     backgroundColor: blue[700],
   },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
 }));
 
-export default function Videos() {
+const Videos = ({title, date, github, youtube, description}) => {
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
 
@@ -48,60 +55,68 @@ export default function Videos() {
   };
 
   return (
-    <Card className={classes.root}>
-      <CardHeader
-        avatar={
-          <Avatar aria-label="React-Jack" className={classes.avatar}>
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="React Blackjack"
-        subheader="April 2021"
-      />
-        <CardMedia
-            // className={classes.media}
-            component='iframe'
-            height="200"
-            title='test'
-            src='https://www.youtube.com/embed/NPp_AITIQ-M'
-        /><CardMedia
-       
-      />
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          A casino style BlackJack game built using a Rails API backend, React/Redux frontend, and a 3rd party API
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="github repo" href="https://github.com/Doode1524/ReactJack-frontend">
-          <GitHubIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </IconButton>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>
-            Write more here!
-          </Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
+    <React.Fragment>
+      <Grid item xs={4}>
+        <Paper className={classes.paper}>
+          <Card className={classes.root}>
+            <CardHeader
+              avatar={
+                <Avatar aria-label="React-Jack" className={classes.avatar}>
+                  R
+                </Avatar>
+              }
+              action={
+                <IconButton aria-label="settings">
+                  <MoreVertIcon />
+                </IconButton>
+              }
+              title={title}
+              subheader={date}
+            />
+            <CardMedia
+              // className={classes.media}
+              component="iframe"
+              height="200"
+              title="test"
+              src={youtube}
+            />
+            <CardMedia />
+            <CardContent>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {description}
+              </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+              <IconButton
+                aria-label="github repo"
+                href={github}
+              >
+                <GitHubIcon />
+              </IconButton>
+              <IconButton aria-label="share">
+                <ShareIcon />
+              </IconButton>
+              <IconButton
+                className={clsx(classes.expand, {
+                  [classes.expandOpen]: expanded,
+                })}
+                onClick={handleExpandClick}
+                aria-expanded={expanded}
+                aria-label="show more"
+              >
+                <ExpandMoreIcon />
+              </IconButton>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography paragraph>Write more here!</Typography>
+              </CardContent>
+            </Collapse>
+          </Card>
+        </Paper>
+      </Grid>
+    </React.Fragment>
   );
-}
+};
+
+export default Videos;
